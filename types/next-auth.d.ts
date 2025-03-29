bcd2a10e26fd73ea361c0d3fab09c793e734a338
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-03-29 11:46:52 +0900

    fix

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,3 +1,7 @@
+// 上書きではなく拡張をするために必要
+// https://zenn.dev/qnighy/articles/9c4ce0f1b68350#%E3%83%A2%E3%82%B8%E3%83%A5%E3%83%BC%E3%83%AB%E6%8B%A1%E5%BC%B5
+export {};
+
 declare module "next-auth" {
 	/**
 	 * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
